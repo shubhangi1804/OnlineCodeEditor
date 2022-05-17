1466aede78f7552676c2164e89f23531325ce87a
Shubhangi Biradar <84662388+shubhangi1804@users.noreply.github.com>
2022-05-17 12:30:36 +0530

    home and about page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import img1 from "./home.png"
+import './Home.css'
 
 function Home() {
   return (
     <>
-    <div>
-      
+    <div class="home">
+      <div class="heading">
+        <h1><em>Online Code Editor</em></h1>
+        <br/>
+        <h4><em>A New Way to Code</em></h4>
+        
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod<br/>
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,<br/>
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.<br/>Lorem ipsum dolor sit amet</p>
+      </div>
+      <div class="heading">
+        <img src={img1}/>
+      </div>
     </div>
     </>
   )
